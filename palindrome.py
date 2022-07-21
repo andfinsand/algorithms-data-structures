4b673c8c0775235faae73d855ba6b282d635b7d8
Andrew Finsand <andfinsand@gmail.com>
2022-07-21 15:26:01 -0700

    Check if palindrome.

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+x = "level"
+
+def solution(inputString):
+    counter = -1
+    for letter in range(0, len(inputString)):
+        if inputString[letter] == inputString[counter]:
+            counter -= 1
+        else:
+            return False
+    return True
+
+print(solution(x))
