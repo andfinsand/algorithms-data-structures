7c658e3828dbd78e29e6e2dd41784e465f33e9f1
Andrew Finsand <andfinsand@gmail.com>
2023-05-30 12:27:55 -0700

    find anagram solved

--- algorithms/check-anagram(incomplete).py
+++ algorithms/check-anagram(incomplete).py
@@ -4,12 +4,23 @@
 entry = "heart"
 
 def anagram(checkEntry , given):
-    listGiven = list(given)
+
+    # convert string to lower case and remove empty spaces
+    convertEntry = checkEntry.lower().replace(" ", "")
+    convertGiven = given.lower().replace(" ", "")
+
+    # check if length is the same
     if len(checkEntry) != len(given):
         return False
-    for x in checkEntry:
-        for xgiven in listGiven:
-            if x == xgiven:
-                listGiven.pop(xgiven)
+
+    # convert to lists and sort
+    sortEntry = sorted(list(convertEntry))
+    sortGiven = sorted(list(convertGiven))
+
+    # compare sorted lists
+    if sortEntry == sortGiven:
+        return True
+    else:
+        return False
 
 print(anagram(entry, 'earth'))
